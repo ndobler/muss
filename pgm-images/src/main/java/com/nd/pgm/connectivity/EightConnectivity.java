2519fa52d4b60b7c211932076ab22872d70d97ff
Nicolás Dobler <nicolas.dobler@gmail.com>
2013-10-28 12:20:32 +0100

    Mejorado tratamiento de indices para no tener que tratar excepciones.

--- pgm-images/src/main/java/com/nd/pgm/connectivity/EightConnectivity.java
+++ pgm-images/src/main/java/com/nd/pgm/connectivity/EightConnectivity.java
@@ -7,13 +7,14 @@ public class EightConnectivity implements IConnectivity {
     @Override
     public int compute(int[][] imageData, int i, int j, int size, IOperator operator) {
         int outPixel = imageData[i][j];
-        // se podria hacer que ii y jj iteren revisando que nunca se arranque antes que 0 y finalice despues del limite superior
-        for (int ii = i - size; ii <= i + size; ii++) {
-            for (int jj = j - size; jj <= j + size; jj++) {
+        int height = imageData.length;
+        int width = imageData[0].length;
+        for (int ii = (i - size) < 0 ? 0 : (i - size); ii <= ((i + size) > height - 1 ? (height - 1) : (i + size)); ii++) {
+            for (int jj = (j - size) < 0 ? 0 : (j - size); jj <= ((j + size) > width - 1 ? (width - 1) : (j + size)); jj++) {
                 try {
                     outPixel = operator.compute(outPixel, imageData[ii][jj]);
                 } catch (IndexOutOfBoundsException ex) {
-                    //esto es ineficiente, ver comentario anterior
+                    assert false : "No deberia saltar " + ex;
                 }
             }
         }
