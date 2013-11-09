da28e825932667e4fa763e8ea003bb2bdfcf516e
Nicolas Dobler <nicolas.dobler@gmail.com>
2013-11-09 14:31:55 +0100

    refactor para extraccion de variables para facilitar comprensión

--- pgm-images/src/main/java/com/nd/pgm/connectivity/EightConnectivity.java
+++ pgm-images/src/main/java/com/nd/pgm/connectivity/EightConnectivity.java
@@ -5,26 +5,35 @@ import com.nd.pgm.operator.IOperator;
 public class EightConnectivity implements IConnectivity {
 
     @Override
-    public int compute(int[][] imageData, int i, int j, int vsize, int hsize, IOperator operator) {
-        assert vsize % 2 == 1 : "vsize must be odd";
-        assert hsize % 2 == 1 : "vsize must be odd";
+    public int compute(int[][] imageData, int i, int j, int vSize, int hSize, IOperator operator) {
+        assert vSize % 2 == 1 : "vsize must be odd";
+        assert hSize % 2 == 1 : "vsize must be odd";
 
         int outPixel = imageData[i][j];
         int height = imageData.length;
         int width = imageData[0].length;
 
-        int hRatio = vsize / 2;
-        int vRatio = hsize / 2;
-        for (int ii = (i - vRatio) < 0 ? 0 : (i - vRatio); ii <= ((i + vRatio) > height - 1 ? (height - 1) : (i + vRatio)); ii++) {
-            for (int jj = (j - hRatio) < 0 ? 0 : (j - hRatio); jj <= ((j + hRatio) > width - 1 ? (width - 1) : (j + hRatio)); jj++) {
+        int hRatio = vSize / 2;
+        int vRatio = hSize / 2; // se podria hacer con un condicional que acepte pares?
+
+        int vStart = i - vRatio < 0 ? 0 : (i - vRatio);
+        int vEnd = i + vRatio > height - 1 ? (height - 1) : (i + vRatio);
+        for (int ii = vStart; ii <= vEnd; ii++) {
+            int hStart = j - hRatio < 0 ? 0 : (j - hRatio);
+            int hEnd = j + hRatio > width - 1 ? (width - 1) : (j + hRatio);
+            for (int jj = hStart; jj <= hEnd; jj++) {
                 try {
-                    outPixel = operator.compute(outPixel, imageData[ii][jj]);
+                    outPixel = computeOperator(operator, outPixel, imageData[ii][jj]);
                 } catch (IndexOutOfBoundsException ex) {
                     assert false : "No deberia saltar " + ex;
                 }
             }
         }
         return outPixel;
+    }
+
+    protected int computeOperator(IOperator operator, int pixel1, int pixel2) {
+        return operator.compute(pixel1, pixel2);
     }
 
 }
