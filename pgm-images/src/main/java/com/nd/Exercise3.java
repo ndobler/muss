e7239ae7a0a165bc40b09f1b9217c7f303e9deaa
Nicolas Dobler <nicolas.dobler@gmail.com>
2013-11-09 14:33:40 +0100

    Agregado ejercicio 3.4

--- pgm-images/src/main/java/com/nd/Exercise3.java
+++ pgm-images/src/main/java/com/nd/Exercise3.java
@@ -1,9 +1,13 @@
 package com.nd;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 import com.nd.pgm.PGMImage;
 import com.nd.pgm.PGMImageException;
+import com.nd.pgm.connectivity.EightConnectivity;
+import com.nd.pgm.connectivity.IConnectivity;
+import com.nd.pgm.operator.IOperator;
 
 /**
  * Exercise 3.1 Implement the following functions to compute an erosion and a dilation using a 3x3 square structuring
@@ -74,16 +78,37 @@ import com.nd.pgm.PGMImageException;
  */
 public class Exercise3 {
 
+    static class NumberOfOperationsCalculator extends EightConnectivity {
+
+        private long operationsCount = 0;
+        private DecimalFormat formatter = new DecimalFormat("###,###.##");
+
+        @Override
+        protected int computeOperator(IOperator operator, int pixel1, int pixel2) {
+            operationsCount++;
+            return 1;
+        }
+
+        public String getOperationsCount() {
+            long res = operationsCount;
+            operationsCount = 0;
+            return formatter.format(res);
+        }
+
+    }
+
     public static void main(String[] args) {
         try {
             System.out.println("3x3 test\n");
             erosionAndDilation3x3Test();
             System.out.println("\n\nnxn test\n");
             erosionAndDilationNMTest();
-            System.out.println("\n\nCompare performance of erosion and dilation using a nxn structuring element or by using n operations with an se size 1\n");
-            performanceComparisonTest();
             System.out.println("\nInversion test\n");
             inversionTest();
+            System.out.println("\n\nCompare performance of erosion and dilation using a nxn structuring element or by using n operations with an se size 1\n");
+            performanceComparisonTest();
+            System.out.println("\nNumber of operations comparison\n");
+            exercise34();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (PGMImageException e) {
@@ -174,9 +199,97 @@ public class Exercise3 {
         outDilation.saveImage("target/dil" + size + ".pgm");
         outDilationOpt.saveImage("target/dil" + size + "opt.pgm");
         System.out.println("compare " + outDilation.equals(outDilationOpt));
-        
-        // number of operations comparison - Exercise 3.4
-        
+
+    }
+
+    /**
+     * Exercise 3.4 In this exercise we are going to compare the number of operations in two alternatives for computing
+     * a morphological dilation with structuring element.
+     * 
+     * Let B be the MxM square structuring element. Let C be the 1xM 1-D horizontal structuring element. x...xXx...x
+     * (Note: the number of pixels is M.) Let D be the Mx1 1-D vertical structuring element. x . . . x X x . . . x
+     * (Note: the number of pixels is M.)
+     * 
+     * 'X' denotes the origin of coordinates or center of the structuring element.
+     * 
+     * It can be observed that B = dilate_C (D) = dilate_D (C).
+     * 
+     * Estimate the number or 'max' operations that must be computed in order to process a NxN square input image using
+     * the following alternatives: dilate_B (I)) dilate_C(dilate_D (I)))
+     * 
+     * Assume that the sizes of B, C and D are, respectively: MxM, 1xM y Mx1. Border effects should not be considered
+     * for simplicity, i.e., all image pixels should be treated in the same manner.
+     * 
+     * @throws PGMImageException
+     */
+    private static void exercise34() throws PGMImageException {
+        computeOperations(256, 1);
+        computeOperations(256, 2);
+        computeOperations(1024, 2);
+        computeOperations(1024, 15);
+        computeOperations(1024 * 5, 16);
+
+    }
+
+    private static void computeOperations(int imageSize, int seSize) throws PGMImageException {
+        DecimalFormat formatter = new DecimalFormat("###,###.##");
+
+        int seDimensions = 2 * seSize + 1;
+        System.out.println("\nResultados para imagen de " + imageSize + "x" + imageSize + " y structuring element size " + seDimensions + "x" + seDimensions);
+        NumberOfOperationsCalculator visitor = new NumberOfOperationsCalculator();
+
+        eroDilSimulator(imageSize, 2 * seSize + 1, 2 * seSize + 1, visitor);
+        System.out.println("El algoritmo completo necesitó " + visitor.getOperationsCount() + " operaciones");
+        long miCalculo = imageSize * imageSize * seDimensions * seDimensions;
+        System.out.println("El calculo sin extremos es de " + formatter.format(miCalculo));
+
+        erosionDilationRecursiveNxM(imageSize, seSize, visitor);
+        System.out.println("El algoritmo recursivo necesitó " + visitor.getOperationsCount() + " operaciones");
+        miCalculo = 0;
+        int i = seSize;
+        while (i-- > 0) {
+            miCalculo += imageSize * imageSize * 3 * 3;
+        }
+        System.out.println("El calculo sin extremos es de " + formatter.format(miCalculo));
+
+        eroDilSimulator(imageSize, seDimensions, 1, visitor);
+        eroDilSimulator(imageSize, 1, seDimensions, visitor);
+        System.out.println("El algoritmo completo con dilaciones 1x" + seDimensions + " y " + seDimensions + "x1 necesitó " + visitor.getOperationsCount()
+                + " operaciones");
+        miCalculo = imageSize * imageSize * seDimensions * 2;
+        System.out.println("El calculo sin extremos es de " + formatter.format(miCalculo));
+
+        // hay algo mal en alguno de los que sigue a continuación, sea el cálculo o la prueba
+        erosionDilationRecursive1MN1(imageSize, seDimensions, visitor);
+        System.out.println("El algoritmo recursivo con " + seSize + " dilaciones 1x3 y 3x1 necesitó " + visitor.getOperationsCount() + " operaciones");
+        miCalculo = 0;
+        i = seSize;
+        while (i-- > 0) {
+            miCalculo += imageSize * imageSize * 3 * 2;
+        }
+        System.out.println("El calculo sin extremos es de " + formatter.format(miCalculo));
+    }
+
+    private static void erosionDilationRecursiveNxM(int imageSize, int size, IConnectivity visitor) throws PGMImageException {
+        while (size-- > 0) {
+            eroDilSimulator(imageSize, 3, 3, visitor);
+        }
+    }
+
+    private static void erosionDilationRecursive1MN1(int imageSize, int size, IConnectivity visitor) throws PGMImageException {
+        while (size-- > 0) {
+            eroDilSimulator(imageSize, 1, 3, visitor);
+            eroDilSimulator(imageSize, 3, 1, visitor);
+        }
+    }
+
+    private static void eroDilSimulator(int imageSize, int sEWidth, int sEHeight, IConnectivity visitor) {
+        int[][] id = new int[imageSize][imageSize];
+        for (int i = 0; i < imageSize; i++) {
+            for (int j = 0; j < imageSize; j++) {
+                visitor.compute(id, i, j, sEHeight, sEWidth, null);
+            }
+        }
     }
 
     private static void inversionTest() throws IOException, PGMImageException {
