e2003ab0d7dd95ccb8c1b7ca55f87f540ad9cff8
Nicolas Dobler <nicolas.dobler@gmail.com>
2014-01-11 22:02:12 +0100

    Implementacion ejercicio 5

--- pgm-images/src/main/java/com/nd/ExercisesPAI.java
+++ pgm-images/src/main/java/com/nd/ExercisesPAI.java
@@ -7,7 +7,7 @@ public class ExercisesPAI {
         Exercise2.main(null);
         Exercise3.main(null);
         Exercise4.main(null);
-        //Exercise5.main(null);
+        Exercise5.main(null);
         Exercise6.main(null);
 
     }
