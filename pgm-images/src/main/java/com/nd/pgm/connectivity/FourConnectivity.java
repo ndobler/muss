b5dd0a7d8a0a4b1e11465b500c25c3e6993f78a1
Nicolas Dobler <nicolas.dobler@gmail.com>
2014-01-11 21:58:40 +0100

    Implementacion four connectivity

--- pgm-images/src/main/java/com/nd/pgm/connectivity/FourConnectivity.java
+++ pgm-images/src/main/java/com/nd/pgm/connectivity/FourConnectivity.java
@@ -5,9 +5,42 @@ import com.nd.pgm.operator.IOperator;
 public class FourConnectivity implements IConnectivity {
 
     @Override
-    public int compute(int[][] imageData, int i, int j, int vsize, int hsize, IOperator operator) {
-        assert false : "pendiente implementacion";
-        return 0;
+    public int compute(int[][] imageData, int i, int j, int vSize, int hSize, IOperator operator) {
+        assert vSize % 2 == 1 : "vsize must be odd";
+        assert hSize % 2 == 1 : "vsize must be odd";
+
+        int outPixel = imageData[i][j];
+        int height = imageData.length;
+        int width = imageData[0].length;
+
+        int hRatio = vSize / 2;
+        int vRatio = hSize / 2; // se podria hacer con un condicional que acepte pares?
+
+        int vStart = i - vRatio < 0 ? 0 : (i - vRatio);
+        int vEnd = i + vRatio > height - 1 ? (height - 1) : (i + vRatio);
+        for (int ii = vStart; ii <= vEnd; ii++) {
+            try {
+                outPixel = computeOperator(operator, outPixel, imageData[ii][j], i, j, ii, j);
+            } catch (IndexOutOfBoundsException ex) {
+                assert false : "No deberia saltar " + ex;
+            }
+        }
+        int hStart = j - hRatio < 0 ? 0 : (j - hRatio);
+        int hEnd = j + hRatio > width - 1 ? (width - 1) : (j + hRatio);
+        for (int jj = hStart; jj <= hEnd; jj++) {
+            if (jj == j)
+                continue;
+            try {
+                outPixel = computeOperator(operator, outPixel, imageData[i][jj], i, j, i, jj);
+            } catch (IndexOutOfBoundsException ex) {
+                assert false : "No deberia saltar " + ex;
+            }
+        }
+
+        return outPixel;
     }
 
+    protected int computeOperator(IOperator operator, int pixel1, int pixel2, int p1y, int p1x, int p2y, int p2x) {
+        return operator.compute(pixel1, pixel2, p1y, p1x, p2y, p2x);
+    }
 }
