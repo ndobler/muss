bee00154fb30f0d161623b6276b22914912d9db3
Nicolas Dobler <nicolas.dobler@gmail.com>
2014-01-11 22:01:02 +0100

    Implementacion ejercicio 5

--- pgm-images/src/main/java/com/nd/Exercise5.java
+++ pgm-images/src/main/java/com/nd/Exercise5.java
@@ -1,5 +1,10 @@
 package com.nd;
 
+import java.io.IOException;
+
+import com.nd.pgm.PGMImage;
+import com.nd.pgm.PGMImageException;
+
 /**
  * Exercise 5.1 As discussed in class, proof that the alternated filter opening-closing (or closing-opening) is
  * idempotent using the properties of openings and closings.
@@ -9,5 +14,38 @@ package com.nd;
  * 
  */
 public class Exercise5 {
+
+    public static void main(String[] args) {
+        try {
+            exercise5();
+        } catch (IOException | PGMImageException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void exercise5() throws PGMImageException, IOException {
+        PGMImage img = new PGMImage("src/test/resources/immed_gray_inv.pgm");
+
+        int size = 1;
+        PGMImage test1 = img.imOpening(size).imClosing(size);
+        PGMImage test2 = test1.imOpening(size).imClosing(size);
+        System.out.println("Is opening/closing size " + size + " idempotent? " + test1.equals(test2));
+
+        size = 10;
+        test1 = img.imOpening(size).imClosing(size);
+        test2 = test1.imOpening(size).imClosing(size);
+        System.out.println("Is opening/closing size " + size + " idempotent? " + test1.equals(test2));
+
+        size = 1;
+        test1 = img.imClosing(size).imOpening(size);
+        test2 = test1.imClosing(size).imOpening(size);
+        System.out.println("Is opening/closing size " + size + " idempotent? " + test1.equals(test2));
+
+        size = 10;
+        test1 = img.imClosing(size).imOpening(size);
+        test2 = test1.imClosing(size).imOpening(size);
+        System.out.println("Is opening/closing size " + size + " idempotent? " + test1.equals(test2));
+
+    }
 
 }
